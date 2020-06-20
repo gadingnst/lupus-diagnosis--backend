7c8883ec0d64d1d5a7ac97ae41bedcc667db8d7d
Gading <sutan.gnst@gmail.com>
2020-06-21 02:50:38 +0700

    delete build folder

--- build/app/Helpers/Http.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Http {
-    static send(res, data) {
-        return res.status(data.code).send(Object.assign(Object.assign({}, data), { error: false }));
-    }
-}
-exports.default = Http;
--- build/app/Helpers/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Http_1 = require("./Http");
-exports.Http = Http_1.default;
-var HttpError_1 = require("./HttpError");
-exports.HttpError = HttpError_1.default;
